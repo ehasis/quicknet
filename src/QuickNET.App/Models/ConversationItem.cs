1d60b80f84d380b0a7ced6bd26412f689df561e1
Edison Henrique Andreassy <ehasis@hotmail.com>
2026-05-29 14:27:54 -0300

    Ajuste fino no tema escuro

--- src/QuickNET.App/Models/ConversationItem.cs
+++ src/QuickNET.App/Models/ConversationItem.cs
@@ -7,5 +7,5 @@ public class ConversationItem
     public string DisplayText { get; set; } = "";
     public bool IsInput { get; set; }
     public bool IsError { get; set; }
-    public IBrush Foreground => IsInput ? Brushes.Gray : IsError ? Brushes.OrangeRed : Brushes.White;
+    public IBrush Foreground => IsInput ? Brushes.DarkGray : IsError ? Brushes.OrangeRed : Brushes.LightGray;
 }
